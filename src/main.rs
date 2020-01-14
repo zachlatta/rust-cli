b96791d6aaac4e1079596eff709e295327f8b718
Zach Latta <zach@zachlatta.com>
2020-01-14 13:38:16 -0800

    Produce nicer errors

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
-use structopt::StructOpt;
+use exitfailure::ExitFailure;
+use failure::ResultExt;
 use std::fs::File;
-use std::io::BufReader;
 use std::io::prelude::*;
+use std::io::BufReader;
+use structopt::StructOpt;
 
 #[derive(StructOpt)]
 struct Cli {
@@ -11,17 +13,19 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
-fn main() {
+fn main() -> Result<(), ExitFailure> {
     let args = Cli::from_args();
 
-    let file = File::open(&args.path).expect("could not find file");
+    let file = File::open(&args.path)
+        .with_context(|_| format!("could not read file `{}`", args.path.display()))?;
     let reader = BufReader::new(file);
 
     for raw_line in reader.lines() {
-        let line = raw_line.expect("error reading line");
+        let line = raw_line.with_context(|_| format!("could not read line"))?;
 
         if line.contains(&args.pattern) {
             println!("{}", line);
         }
     }
+    Ok(())
 }
